a4da443c6f81765b58a803039abf3e5d1d530e8e
PLoganathan <loganathanp.avancer@gmail.com>
2019-08-08 17:03:39 +0530

    New class added and edit the old class

---
+++ src/test/java/screenshot/AddNewClass.java
@@ -0,0 +1,8 @@
+package screenshot;
+
+public class AddNewClass {
+	
+	//Testing git and git Hub. 2 nd round of added new class. 
+	//add, status , commit and push --> afer see your remote repository
+
+}
